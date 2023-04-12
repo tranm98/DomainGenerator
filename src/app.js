7d5ec32ae5d6fc70d32e74c0cfefeebb3688bcc6
Mai Tran <t.mai.1998@gmail.com>
2023-04-12 20:10:13 +0000

    move code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pro = ['the', 'our', 'my', 'we'];
+  let adj = ['super', 'great', 'cool', 'charming', 'giant'];
+  let noun = ['cat', 'tech', 'blanket', 'dog', 'computing' ];
+  let ext = ['.com', '.org', '.net', '.us'];
+
+  for (let i = 0; i < pro.length; i++){
+      for(let j = 0; j < adj.length; j++){
+          for(let k = 0; k < noun.length; k++){
+              for(let l = 0; l < ext.length; l++){
+                  domainName.push(" " + pro[i] + adj[j] + noun[k] + ext[l]);
+              }
+          }
+      }
+  console.log(domainName);
+  };
 };
